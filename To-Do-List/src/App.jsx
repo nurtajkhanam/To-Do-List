fab67fc14a42a35e3250baae313d326286f31dd4
nurtaj <nurtajkhanam777@gmail.com>
2024-09-13 19:10:05 +0600

    todo list app

---
+++ To-Do-List/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+const App = () => {
+  return (
+    <div>
+      <center className="todo-content">
+        <h1>To-Do App</h1></center>
+        <div class="container text-center">
+          <div class="row">
+            <div class="col-8">col-8</div>
+            <div class="col-4">col-4</div>
+          </div>
+        </div>
+    </div>
+  );
+};
+
+export default App;
